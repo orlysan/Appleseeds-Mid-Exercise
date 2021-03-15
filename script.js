2ff93ec8d377c44f339eaa3a362d2383a7869e30
orly <orlysan1@gmail.com>
2021-03-15 10:57:26 +0200

    script

---
+++ script.js
@@ -0,0 +1,16 @@
+function changrBnr(){
+    var advTitle = document.getElementById('adv-title');  
+    var advImg = document.getElementById('adv-img');
+    
+
+ 
+   
+setInterval(function(){ 
+    advTitle.innerHTML = "טיפז";
+    advImg.src= "images/250X110668568400.jpg";
+    }, 5000);
+
+//<h3 id="adv-title3">yello click&pick;</h3>
+//<img id="adv-img2" src= alt="">
+                        //<img id="adv-img3" src="images/250X110571325734.jpg alt="></img>
+}
